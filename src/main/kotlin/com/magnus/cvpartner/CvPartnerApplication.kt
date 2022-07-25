3378af7848ca66ea1f96c33854e1c95b0a3f98e2
Magnus Kragseth <magnus.kragseth@gmail.com>
2022-07-25 19:00:05 +0200

    Spring init

---
+++ src/main/kotlin/com/magnus/cvpartner/CvPartnerApplication.kt
@@ -0,0 +1,11 @@
+package com.magnus.cvpartner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CvPartnerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CvPartnerApplication>(*args)
+}
---
+++ src/test/kotlin/com/magnus/cvpartner/CvPartnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.magnus.cvpartner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CvPartnerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
